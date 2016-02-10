4abb3fe2c57b13fc13ae8c7bb919db9e84d69cf2
seleb <sean.s.leblanc@gmail.com>
2016-02-09 19:46:47 -0500

    more setup

--- Game/Source/Game/include/MY_Scene_Main.h
+++ Game/Source/Game/include/MY_Scene_Main.h
@@ -6,9 +6,41 @@ class StereoCamera;
 
 class MY_Scene_Main : public MY_Scene_Base{
 public:
+	// The scene's physics world
+	BulletWorld * bulletWorld;
+	// used to draw wireframes showing physics colliders, transforms, etc
+	BulletDebugDrawer * bulletDebugDrawer;
+
+
+	// ID of the currently playing audio track
+	unsigned long int currentTrackId;
+	// currently playing audio track
+	OpenAL_Sound * currentTrack;
+	// increments currentTrackId and replaces the current track with the one located at the new ID
+	void getNextTrack();
+
+
+
 	StereoCamera * vrCam;
+	// whether we are paused and waiting for user input in order to proceed
+	bool waitingForInput;
 
 	MY_Scene_Main(Game * _game);
 
 	virtual void render(sweet::MatrixStack * _matrixStack, RenderOptions * _renderOptions) override;
+	virtual void update(Step * _step) override;
+	
+	// overriden to add physics debug drawing
+	virtual void enableDebug() override;
+	// overriden to remove physics debug drawing
+	virtual void disableDebug() override;
+
+
+	// updates currentHoverTarget based on raycasting
+	void updateHoverTarget(Step * _step);
+	NodeBulletBody * currentHoverTarget;
+	// length of time the currenHoverTarget has been hovered
+	float hoverTime;
+	// length of time a target must be hovered before it is chosen as a selection
+	float targetHoverTime;
 };
--- Game/Source/Game/src/MY_Scene_Main.cpp
+++ Game/Source/Game/src/MY_Scene_Main.cpp
@@ -8,7 +8,17 @@
 
 MY_Scene_Main::MY_Scene_Main(Game * _game) :
 	MY_Scene_Base(_game),
-	vrCam(new StereoCamera())
+	vrCam(new StereoCamera()),
+
+	currentHoverTarget(nullptr),
+	hoverTime(0),
+	targetHoverTime(1.5f),
+
+	bulletWorld(new BulletWorld(glm::vec3(0, -9.8, 0))), // we initialize the world's gravity here
+	bulletDebugDrawer(new BulletDebugDrawer(bulletWorld->world)),
+
+	currentTrack(nullptr),
+	currentTrackId(0)
 {
 	MeshInterface * chairMesh = MY_ResourceManager::globalAssets->getMesh("chair")->meshes.at(0);
 	chairMesh->pushTexture2D(MY_ResourceManager::globalAssets->getTexture("chair")->texture);
@@ -27,6 +37,10 @@ MY_Scene_Main::MY_Scene_Main(Game * _game) :
 	// add a cubemap (cubemaps use a special texture type and shader component. these can be instantiated separately if desired, but the CubeMap class handles them both for us)
 	CubeMap * cubemap = new CubeMap("assets/textures/cubemap", "png");
 	childTransform->addChild(cubemap);
+
+
+	// start the experience
+	getNextTrack();
 }
 
 void MY_Scene_Main::render(sweet::MatrixStack * _matrixStack, RenderOptions * _renderOptions){
@@ -37,4 +51,96 @@ void MY_Scene_Main::render(sweet::MatrixStack * _matrixStack, RenderOptions * _r
 	if(sweet::ovrInitialized){
 		vrCam->blitTo(0);
 	}
+}
+
+void MY_Scene_Main::update(Step * _step){
+	if(waitingForInput){
+		updateHoverTarget(_step);
+
+		// check for selection
+		if(hoverTime >= targetHoverTime){
+			// make a selection and move on to the next track
+			// TODO
+			getNextTrack();
+		}
+	}else{
+		// update the active audio stream
+		currentTrack->update(_step);
+
+		// update the position of the active audio stream
+		// TODO
+	}
+
+	// update the listener's position/orientation based on the camera
+	NodeOpenAL::setListener(vrCam, true);
+
+	// update the scene
+	MY_Scene_Base::update(_step);
+}
+
+
+void MY_Scene_Main::getNextTrack(){
+	// stop the old track and remove it from the scene
+	if(currentTrack != nullptr){
+		currentTrack->stop();
+		childTransform->removeChild(currentTrack);
+	}
+
+	++currentTrackId;
+	std::stringstream ss;
+	ss << "track" << currentTrackId;
+	currentTrack = MY_ResourceManager::globalAssets->getAudio(ss.str())->sound;
+	
+	// add the new track to the scene and play it
+	childTransform->addChild(currentTrack, false);
+	currentTrack->play(true); // (shouldn't actually loop in the final, just for testing)
+}
+
+
+void MY_Scene_Main::updateHoverTarget(Step * _step){
+	float range = 100;
+	glm::vec3 pos = activeCamera->childTransform->getWorldPos();
+	btVector3 start(pos.x, pos.y, pos.z);
+	btVector3 dir(activeCamera->forwardVectorRotated.x, activeCamera->forwardVectorRotated.y, activeCamera->forwardVectorRotated.z);
+	btVector3 end = start + dir*range;
+	btCollisionWorld::ClosestRayResultCallback RayCallback(start, end);
+	bulletWorld->world->rayTest(start, end, RayCallback);
+		
+		
+	NodeBulletBody * lastHoverTarget = currentHoverTarget;
+	
+	// if we're not looking at anything, cancel the current hover target
+	if(!RayCallback.hasHit()){
+		currentHoverTarget = nullptr;
+		return;
+	}
+
+	// if what we're looking at isn't a valid target, cancel the current hover target
+	NodeBulletBody * me = static_cast<NodeBulletBody *>(RayCallback.m_collisionObject->getUserPointer());
+	if(me != nullptr){
+		currentHoverTarget = nullptr;
+		return;
+	}
+
+	// if we're looking at the same target as before, increment towards selection
+	if(currentHoverTarget == lastHoverTarget && lastHoverTarget != nullptr){
+		hoverTime += _step->deltaTime;
+		return;
+	}
+
+	// if we're looking at a new target, restart
+	currentHoverTarget = me;
+	hoverTime = 0;
+}
+
+
+
+void MY_Scene_Main::enableDebug(){
+	MY_Scene_Base::enableDebug();
+	bulletDebugDrawer->setDebugMode(btIDebugDraw::DBG_MAX_DEBUG_DRAW_MODE);
+	childTransform->addChildAtIndex(bulletDebugDrawer, -1, false); // make sure the debug drawer is the last thing drawn
+}
+void MY_Scene_Main::disableDebug(){
+	MY_Scene_Base::disableDebug();
+	bulletDebugDrawer->setDebugMode(btIDebugDraw::DBG_NoDebug);
 }
