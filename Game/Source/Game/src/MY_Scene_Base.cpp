399a6c2bbfc16b5a55d6d3fc395bd4d425141821
seleb <sean.s.leblanc@gmail.com>
2016-03-27 18:35:41 -0400

    put text colour back to normal

--- Game/Source/Game/src/MY_Scene_Base.cpp
+++ Game/Source/Game/src/MY_Scene_Base.cpp
@@ -34,7 +34,7 @@ MY_Scene_Base::MY_Scene_Base(Game * _game) :
 	baseShader->addComponent(new ShaderComponentTexture(baseShader, 0.1f));
 	baseShader->compileShader();
 
-	textShader->textComponent->setColor(glm::vec4(255.0f, 1.0f, 0.0f, 255.f));
+	textShader->textComponent->setColor(glm::vec4(0,0,0, 255.f));
 
 	// Set up debug camera
 	cameras.push_back(debugCam); // Add it to the cameras list (this isn't strictly necessary, but is useful organizational and debugging purposes)
